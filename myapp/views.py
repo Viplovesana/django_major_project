e4d61af728083a6ddc901487ff3b38bfcc62056e
Viplovesana <viplovesana90@gmail.com>
2025-07-29 16:27:20 +0530

    new commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from.models import User,Query
 
 # Create your views here.
@@ -97,3 +97,4 @@ def delete(req,id,pk):
 
 
 
+ 
